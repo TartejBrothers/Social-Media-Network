0b16948710ba1ea9f21608a2c770074f8b0affaf
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-10-29 19:51:57 +0530

    All

--- gui.c
+++ gui.c
@@ -3,19 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Structure to represent a user
 typedef struct User {
     char name[50];
     struct User* next;
 } User;
 
-// Structure to represent an edge (connection)
+
 typedef struct Edge {
     User* user;
     struct Edge* next;
 } Edge;
 
-// Structure to represent a social network graph
+
 typedef struct Graph {
     User* users;
     Edge** adjacency_list;
@@ -24,7 +23,7 @@ typedef struct Graph {
 
 Graph* graph;
 
-// Function to create a new user
+
 User* createUser(const char* name) {
     User* user = (User*)malloc(sizeof(User));
     strcpy(user->name, name);
@@ -32,7 +31,7 @@ User* createUser(const char* name) {
     return user;
 }
 
-// Function to create a new edge (connection)
+
 Edge* createEdge(User* user) {
     Edge* edge = (Edge*)malloc(sizeof(Edge));
     edge->user = user;
@@ -40,18 +39,17 @@ Edge* createEdge(User* user) {
     return edge;
 }
 
-// Function to initialize a social network graph
 Graph* createGraph(int num_users) {
     Graph* graph = (Graph*)malloc(sizeof(Graph));
     graph->users = NULL;
     graph->num_users = num_users;
     graph->adjacency_list = (Edge**)malloc(num_users * sizeof(Edge*));
     for (int i = 0; i < num_users; i++) {
-        graph->adjacency_list[i] = NULL; // Initialize to NULL
+        graph->adjacency_list[i] = NULL; 
     }
     return graph;
 }
-// Function to search for a user by name and return a pointer to the user
+
 User* searchUserInGraph(Graph* graph, const char* username) {
     User* user = graph->users;
     while (user) {
@@ -64,7 +62,7 @@ User* searchUserInGraph(Graph* graph, const char* username) {
 }
 
 
-// Function to add a user to the graph
+
 void addUser(Graph* graph, const char* name) {
     User* newUser = createUser(name);
     newUser->next = graph->users;
@@ -72,12 +70,12 @@ void addUser(Graph* graph, const char* name) {
     printf("User '%s' added.\n", name);
 }
 
-// Function to add a connection between two users
+
 void addConnection(Graph* graph, const char* user1, const char* user2) {
     User* u1 = NULL;
     User* u2 = NULL;
 
-    // Find user1 and user2 in the list of users
+
     User* current = graph->users;
     while (current) {
         if (strcmp(current->name, user1) == 0) {
@@ -93,7 +91,7 @@ void addConnection(Graph* graph, const char* user1, const char* user2) {
         return;
     }
 
-    // Create edges to represent the connection
+
     Edge* edge1 = createEdge(u2);
     edge1->next = graph->adjacency_list[u1 - graph->users];
     graph->adjacency_list[u1 - graph->users] = edge1;
@@ -104,8 +102,7 @@ void addConnection(Graph* graph, const char* user1, const char* user2) {
      printf("Connection between '%s' and '%s' made.\n", user1, user2);
 }
 
-// Function to display all users and their connections
-// Function to display all users and their connections
+
 void displayGraph(Graph* graph, GtkTextBuffer *buffer) {
     User* user = graph->users;
     while (user) {
@@ -127,30 +124,27 @@ void displayGraph(Graph* graph, GtkTextBuffer *buffer) {
 }
 
 
-// Callback function for adding a user
+
 void addUserCallback(GtkWidget *widget, gpointer data) {
     const char *username = gtk_entry_get_text(GTK_ENTRY(data));
     if (strlen(username) > 0) {
         addUser(graph, username);
-        // Update the user interface if needed
     }
 }
 
-// Callback function for creating a connection
-// Callback function for creating a connection
+
 void addConnectionCallback(GtkWidget *widget, gpointer data) {
     const char *connection = gtk_entry_get_text(GTK_ENTRY(data));
     char user1[50], user2[50];
 
-    // Parse the input and check for valid input
     if (sscanf(connection, "%s %s", user1, user2) == 2) {
-        // Check if both users exist in the graph
+
         User* u1 = searchUserInGraph(graph, user1);
         User* u2 = searchUserInGraph(graph, user2);
 
         if (u1 != NULL && u2 != NULL) {
             addConnection(graph, user1, user2);
-            // Update the user interface if needed
+   
         } else {
             printf("User not found!\n");
         }
@@ -160,17 +154,14 @@ void addConnectionCallback(GtkWidget *widget, gpointer data) {
 }
 
 
-// Callback function for displaying the network
-// Callback function for displaying the network
-// Callback function for displaying the network
 void displayGraphCallback(GtkWidget *widget, gpointer data) {
-    // Create a new window to display the network
+
     GtkWidget *window;
     GtkWidget *text_view;
     GtkWidget *scrolled_window;
     GtkTextBuffer *buffer;
 
-    // Initialize GTK
+
     gtk_init(NULL, NULL);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -180,10 +171,10 @@ void displayGraphCallback(GtkWidget *widget, gpointer data) {
     text_view = gtk_text_view_new();
     buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
 
-    // Clear the existing text in the buffer
+
     gtk_text_buffer_set_text(buffer, "", -1);
 
-    // Display the network in the text view
+
     displayGraph(graph, buffer);
 
     scrolled_window = gtk_scrolled_window_new(NULL, NULL);
@@ -192,7 +183,7 @@ void displayGraphCallback(GtkWidget *widget, gpointer data) {
 
     gtk_widget_show_all(window);
 
-    // Start the GTK main loop
+
     gtk_main();
 }
 
@@ -208,21 +199,21 @@ int main(int argc, char *argv[]) {
     GtkWidget *addConnectionButton;
     GtkWidget *displayNetworkButton;
 
-    // Initialize GTK
+
     gtk_init(&argc, &argv);
 
-    // Initialize the graph
+
     graph = createGraph(100);
 
-    // Create the main window
+
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Social Network");
 
-    // Create a vertical box to hold widgets
+
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    // Create labels and entries for adding users and connections
+
     label1 = gtk_label_new("Enter username to add:");
     entry1 = gtk_entry_new();
     label2 = gtk_label_new("Enter User1 User2 for connection:");
@@ -230,15 +221,15 @@ int main(int argc, char *argv[]) {
     addUserButton = gtk_button_new_with_label("Add User");
     addConnectionButton = gtk_button_new_with_label("Create Connection");
 
-    // Connect the callbacks to the buttons
+
     g_signal_connect(G_OBJECT(addUserButton), "clicked", G_CALLBACK(addUserCallback), entry1);
     g_signal_connect(G_OBJECT(addConnectionButton), "clicked", G_CALLBACK(addConnectionCallback), entry2);
 
-    // Create a button to display the network
+
     displayNetworkButton = gtk_button_new_with_label("Display Network");
     g_signal_connect(G_OBJECT(displayNetworkButton), "clicked", G_CALLBACK(displayGraphCallback), NULL);
 
-    // Add widgets to the box
+
     gtk_box_pack_start(GTK_BOX(vbox), label1, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), entry1, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), addUserButton, FALSE, FALSE, 0);
@@ -247,13 +238,13 @@ int main(int argc, char *argv[]) {
     gtk_box_pack_start(GTK_BOX(vbox), addConnectionButton, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), displayNetworkButton, FALSE, FALSE, 0);
 
-    // Display the window
+
     gtk_widget_show_all(window);
 
-    // Start the GTK main loop
+
     gtk_main();
 
-    // Clean up memory (you should free all allocated memory before exiting)
+
     free(graph->adjacency_list);
     free(graph);
 
